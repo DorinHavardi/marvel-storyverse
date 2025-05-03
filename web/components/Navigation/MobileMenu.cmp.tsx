ce6928e16ef584050761800377936e52c40dc077
dorinhavardi <dorin.havardi@gmail.com>
2025-05-03 11:35:48 +0300

    animation for mobile nav

--- web/components/Navigation/MobileMenu.cmp.tsx
+++ web/components/Navigation/MobileMenu.cmp.tsx
@@ -11,11 +11,10 @@ export const MobileMenu = () => {
 
   return (
     <>
-      {/* כפתור תפריט ראשוני */}
       {!open && (
         <button
           onClick={() => setOpen(true)}
-          className="p-2 focus:outline-none"
+          className="p-2 focus:outline-none z-50 relative"
         >
           <svg
             className="w-8 h-8"
@@ -33,43 +32,43 @@ export const MobileMenu = () => {
           </svg>
         </button>
       )}
-
-      {open && (
-        <div className="absolute top-0 right-0 w-1/2 bg-white/10 backdrop-blur-md rounded-l-lg rounded-tl-none z-50 p-4 flex flex-col gap-6 drop-shadow-lg">
-          {/* כפתור סגירה בתוך התפריט */}
-          <button
-            onClick={() => setOpen(false)}
-            className="self-start p-2 focus:outline-none"
+      <div
+        className={`absolute top-0 right-0 w-1/2 bg-white/10 backdrop-blur-md rounded-l-lg z-40 p-4 flex flex-col gap-6 drop-shadow-lg transition-all duration-300 ease-in-out transform ${
+          open
+            ? 'translate-x-0 opacity-100'
+            : 'translate-x-full opacity-0 pointer-events-none'
+        }`}
+      >
+        <button
+          onClick={() => setOpen(false)}
+          className="self-start p-2 focus:outline-none"
+        >
+          <svg
+            className="w-8 h-8"
+            fill="none"
+            stroke="white"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              className="w-8 h-8"
-              fill="none"
-              stroke="white"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-
-          {/* לינקים */}
-          {NAVIGATION_ROUTES.map(link => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className="text-white text-2xl"
-              onClick={() => setOpen(false)}
-            >
-              {t(link.labelKey)}
-            </Link>
-          ))}
-        </div>
-      )}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        {NAVIGATION_ROUTES.map(link => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-white text-2xl"
+            onClick={() => setOpen(false)}
+          >
+            {t(link.labelKey)}
+          </Link>
+        ))}
+      </div>
     </>
   );
 };
