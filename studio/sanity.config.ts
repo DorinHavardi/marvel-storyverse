06d7271a97c45cb3801a543d198d29e086eabda5
dorinhavardi <dorin.havardi@gmail.com>
2025-05-17 09:15:56 +0300

    added origin

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,16 +3,21 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
+const allowedOrigin = process.env.SANITY_ALLOWED_ORIGIN || 'http://localhost:3000'
+
 export default defineConfig({
   name: 'default',
   title: 'Marvel Storyverse',
-
   projectId: 'dq10xpy2',
   dataset: 'production',
-
   plugins: [structureTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
+  },
+  api: {
+    cors: {
+      origins: [allowedOrigin],
+      credentials: true,
+    },
   },
 })
